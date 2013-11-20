40eb40b9966f2474aa6f35bf1bea7b56697f1c2f
Jennae Steinmiller <c15jennae.steinmiller@usafa.edu>
2013-11-19 20:26:39 -0700

    All directions working. Time adjustment and code clean up needed

--- main.c
+++ main.c
@@ -77,12 +77,38 @@ int main(void) {
 		TA1CCTL1 &= ~OUTMOD_7;
 		TA1CCTL0 &= ~OUTMOD_7;
 
-		//to move backward
+		//to move backward - working!
 		TA0CCTL1 |= OUTMOD_5;		//set to reset
 		TA0CCTL0 |= OUTMOD_4;		//set to toggle
 		TA1CCTL1 |= OUTMOD_5;		//set to reset
 		TA1CCTL0 |= OUTMOD_4;		//set to toggle
 
+
+		__delay_cycles(1000000);
+
+		TA0CCTL1 &= ~OUTMOD_7;        // set TACCTL1 to Reset / Set mode
+		TA0CCTL0 &= ~OUTMOD_7;			//set to Reset
+		TA1CCTL1 &= ~OUTMOD_7;
+		TA1CCTL0 &= ~OUTMOD_7;
+
+		//to move left
+		TA0CCTL1 |= OUTMOD_7;        // set TACCTL1 to Reset / Set mode
+		TA0CCTL0 |= OUTMOD_5;			//set to Reset
+		TA1CCTL1 |= OUTMOD_5;
+		TA1CCTL0 |= OUTMOD_7;
+
+		__delay_cycles(1000000);
+
+		TA0CCTL1 &= ~OUTMOD_7;        // set TACCTL1 to Reset / Set mode
+		TA0CCTL0 &= ~OUTMOD_7;			//set to Reset
+		TA1CCTL1 &= ~OUTMOD_7;
+		TA1CCTL0 &= ~OUTMOD_7;
+
+		//to move right
+		TA0CCTL1 |= OUTMOD_5;        // set TACCTL1 to Reset / Set mode
+		TA0CCTL0 |= OUTMOD_7;			//set to Reset
+		TA1CCTL1 |= OUTMOD_7;
+		TA1CCTL0 |= OUTMOD_5;
 
 		__delay_cycles(1000000);
 	}
